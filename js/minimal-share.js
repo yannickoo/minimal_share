aeaaed14cadc06927267be27993b83dbfdfcc660
yannickoo <drupal@yannickoo.de>
2014-11-07 16:43:41 +0100

    adds print functionality

--- js/minimal-share.js
+++ js/minimal-share.js
@@ -5,11 +5,15 @@
       $('.minimal-share a', context).once('minimal-share', function() {
         var $this = $(this);
 
-        var width = $this.data('width'), height = $this.data('height'), url = $this.attr('href');
-        var wx = (screen.width - width) >> 1, wy = (screen.height - height) >> 1;
-
         $this.bind('click', function(e) {
-          window.open(url, '', "top=" + wy + ",left=" + wx + ",width=" + width + ",height=" + height);
+          if ($this.hasClass('print')) {
+            window.print();
+          }
+          else {
+            var width = $this.data('width'), height = $this.data('height'), url = $this.attr('href');
+            var wx = (screen.width - width) >> 1, wy = (screen.height - height) >> 1;
+            window.open(url, '', "top=" + wy + ",left=" + wx + ",width=" + width + ",height=" + height);
+          }
 
           e.preventDefault();
         });
