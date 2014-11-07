60cdeea7aed6d952b343422a2f52e1cb0470c1c2
yannickoo <drupal@yannickoo.de>
2014-11-08 00:44:57 +0100

    updates implementation example

--- minimal_share.api.php
+++ minimal_share.api.php
@@ -14,11 +14,15 @@
  *   An associative array of all defined services.
  */
 function hook_minimal_share_services() {
+  $services = array();
+
   $services['drupalorg'] = array(
     'title' => t('Drupal.org'),
     'link' => 'https://www.drupal.org/share.php?u=[url]&t=[title]',
     'size' => array('width' => '600', 'height' => '500'),
   );
+
+  return $services;
 }
 
 
