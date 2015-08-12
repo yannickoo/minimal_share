a228fd70e5c15ed23b02be17b5d80d6814cba178
yannickoo <github@yannick.md>
2015-08-12 13:15:52 +0200

    adds whatsapp service, fixes #11

--- js/minimal-share.js
+++ js/minimal-share.js
@@ -2,8 +2,14 @@
 
   Drupal.behaviors.minimalShare = {
     attach: function (context, settings) {
+      var mobileDevice = (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+
       $('.minimal-share a', context).once('minimal-share', function() {
         var $this = $(this);
+
+        if ($this.hasClass('ms-mobile-only') && !mobileDevice) {
+          $this.addClass('ms-hidden');
+        }
 
         $this.bind('click', function(e) {
           if ($this.hasClass('print')) {
